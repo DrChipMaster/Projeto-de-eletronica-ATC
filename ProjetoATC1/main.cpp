d4458aa5adb012e7399cf29740b063527963afb8
DrChipMaster <andrecampos1998@gmail.com>
2017-12-04 23:52:44 +0000

    JOOOOOOAAAAAAAAAAAAANIS

--- ProjetoATC1/main.cpp
+++ ProjetoATC1/main.cpp
@@ -935,7 +935,7 @@ void verifica_acidente(struct carro *car, struct carro *car1, SDL_Texture *gacid
 		(*car1).velocidadeY = 0;
 
 	}// teste exemplo
-	//ollaaaaaaaaaza
+	//puika
 	//teste 2
 
 	//oi
